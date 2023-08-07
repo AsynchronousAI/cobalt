39a65afe0febb5b1ee39158a2699463fc5199c81
AsynchronousAI <MY_NAME@example.com>
2023-08-07 17:56:31 -0500

    other-platforms

--- cobalt-1.0.0/src/winbuild.bat
+++ cobalt-1.0.0/src/winbuild.bat
@@ -11,9 +11,9 @@
         :: Removes trailing backslash
         :: to enhance readability in the following steps
         set work_dir=%work_dir:~0,-1%
-        set lua_install_dir=%work_dir%\lua
-        set compiler_bin_dir=%work_dir%\tdm-gcc\bin
-        set lua_build_dir=%work_dir%\lua-%lua_version%
+        set lua_install_dir=%work_dir%
+        set compiler_bin_dir=%work_dir%
+        set lua_build_dir=%work_dir%
         set path=%compiler_bin_dir%;%path%
 
         cd /D %lua_build_dir%

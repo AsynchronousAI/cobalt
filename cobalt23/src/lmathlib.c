b6de86641e94c956bfa5f8cbaab9fe17c7bc0a83
AsynchronousAI <aarav.sethi@outlook.com>
2023-08-22 22:17:52 -0500

    Math+

--- cobalt23/src/lmathlib.c
+++ cobalt23/src/lmathlib.c
@@ -702,6 +702,362 @@ static int math_log10 (lua_State *L) {
 
 
 
+// MATHx (lmathx)
+#define MYNAME		"mathx"
+
+#define A(i)	luaL_checknumber(L,i)
+#define I(i)	((int)luaL_checkinteger(L,i))
+
+#undef PI
+#define PI	(l_mathop(3.141592653589793238462643383279502884))
+#define	rad(x)	((x)*(PI/l_mathop(180.0)))
+#define	deg(x)	((x)*(l_mathop(180.0)/PI))
+
+static int Lfmax(lua_State *L)			/** fmax */
+{
+ int i,n=lua_gettop(L);
+ lua_Number m=A(1);
+ for (i=2; i<=n; i++) m=l_mathop(fmax)(m,A(i));
+ lua_pushnumber(L,m);
+ return 1;
+}
+
+static int Lfmin(lua_State *L)			/** fmin */
+{
+ int i,n=lua_gettop(L);
+ lua_Number m=A(1);
+ for (i=2; i<=n; i++) m=l_mathop(fmin)(m,A(i));
+ lua_pushnumber(L,m);
+ return 1;
+}
+
+static int Lfrexp(lua_State *L)			/** frexp */
+{
+ int e;
+ lua_pushnumber(L,l_mathop(frexp)(A(1),&e));
+ lua_pushinteger(L,e);
+ return 2;
+}
+
+static int Lldexp(lua_State *L)			/** ldexp */
+{
+ lua_pushnumber(L,l_mathop(ldexp)(A(1),I(2)));
+ return 1;
+}
+
+static int Lmodf(lua_State *L)			/** modf */
+{
+ lua_Number ip;
+ lua_Number fp=l_mathop(modf)(A(1),&ip);
+ lua_pushnumber(L,ip);
+ lua_pushnumber(L,fp);
+ return 2;
+}
+
+static int Lfabs(lua_State *L)			/** fabs */
+{
+ lua_pushnumber(L,l_mathop(fabs)(A(1)));
+ return 1;
+}
+
+static int Lacos(lua_State *L)			/** acos */
+{
+ lua_pushnumber(L,l_mathop(acos)(A(1)));
+ return 1;
+}
+
+static int Lacosh(lua_State *L)			/** acosh */
+{
+ lua_pushnumber(L,l_mathop(acosh)(A(1)));
+ return 1;
+}
+
+static int Lasin(lua_State *L)			/** asin */
+{
+ lua_pushnumber(L,l_mathop(asin)(A(1)));
+ return 1;
+}
+
+static int Lasinh(lua_State *L)			/** asinh */
+{
+ lua_pushnumber(L,l_mathop(asinh)(A(1)));
+ return 1;
+}
+
+static int Latan(lua_State *L)			/** atan */
+{
+ int n=lua_gettop(L);
+ if (n==1)
+  lua_pushnumber(L,l_mathop(atan)(A(1)));
+ else
+  lua_pushnumber(L,l_mathop(atan2)(A(1),A(2)));
+ return 1;
+}
+
+static int Latan2(lua_State *L)			/** atan2 */
+{
+ lua_pushnumber(L,l_mathop(atan2)(A(1),A(2)));
+ return 1;
+}
+
+static int Latanh(lua_State *L)			/** atanh */
+{
+ lua_pushnumber(L,l_mathop(atanh)(A(1)));
+ return 1;
+}
+
+static int Lcbrt(lua_State *L)			/** cbrt */
+{
+ lua_pushnumber(L,l_mathop(cbrt)(A(1)));
+ return 1;
+}
+
+static int Lceil(lua_State *L)			/** ceil */
+{
+ lua_pushnumber(L,l_mathop(ceil)(A(1)));
+ return 1;
+}
+
+static int Lcopysign(lua_State *L)		/** copysign */
+{
+ lua_pushnumber(L,l_mathop(copysign)(A(1),A(2)));
+ return 1;
+}
+
+static int Lcos(lua_State *L)			/** cos */
+{
+ lua_pushnumber(L,l_mathop(cos)(A(1)));
+ return 1;
+}
+
+static int Lcosh(lua_State *L)			/** cosh */
+{
+ lua_pushnumber(L,l_mathop(cosh)(A(1)));
+ return 1;
+}
+
+static int Ldeg(lua_State *L)			/** deg */
+{
+ lua_pushnumber(L,deg(A(1)));
+ return 1;
+}
+
+static int Lerf(lua_State *L)			/** erf */
+{
+ lua_pushnumber(L,l_mathop(erf)(A(1)));
+ return 1;
+}
+
+static int Lerfc(lua_State *L)			/** erfc */
+{
+ lua_pushnumber(L,l_mathop(erfc)(A(1)));
+ return 1;
+}
+
+static int Lexp(lua_State *L)			/** exp */
+{
+ lua_pushnumber(L,l_mathop(exp)(A(1)));
+ return 1;
+}
+
+static int Lexp2(lua_State *L)			/** exp2 */
+{
+ lua_pushnumber(L,l_mathop(exp2)(A(1)));
+ return 1;
+}
+
+static int Lexpm1(lua_State *L)			/** expm1 */
+{
+ lua_pushnumber(L,l_mathop(expm1)(A(1)));
+ return 1;
+}
+
+static int Lfdim(lua_State *L)			/** fdim */
+{
+ lua_pushnumber(L,l_mathop(fdim)(A(1),A(2)));
+ return 1;
+}
+
+static int Lfloor(lua_State *L)			/** floor */
+{
+ lua_pushnumber(L,l_mathop(floor)(A(1)));
+ return 1;
+}
+
+static int Lfma(lua_State *L)			/** fma */
+{
+ lua_pushnumber(L,l_mathop(fma)(A(1),A(2),A(3)));
+ return 1;
+}
+
+static int Lfmod(lua_State *L)			/** fmod */
+{
+ lua_pushnumber(L,l_mathop(fmod)(A(1),A(2)));
+ return 1;
+}
+
+static int Lgamma(lua_State *L)			/** gamma */
+{
+ lua_pushnumber(L,l_mathop(tgamma)(A(1)));
+ return 1;
+}
+
+static int Lhypot(lua_State *L)			/** hypot */
+{
+ lua_pushnumber(L,l_mathop(hypot)(A(1),A(2)));
+ return 1;
+}
+
+static int Lisfinite(lua_State *L)		/** isfinite */
+{
+ lua_pushboolean(L,isfinite(A(1)));
+ return 1;
+}
+
+static int Lisinf(lua_State *L)			/** isinf */
+{
+ lua_pushboolean(L,isinf(A(1)));
+ return 1;
+}
+
+static int Lisnan(lua_State *L)			/** isnan */
+{
+ lua_pushboolean(L,isnan(A(1)));
+ return 1;
+}
+
+static int Lisnormal(lua_State *L)		/** isnormal */
+{
+ lua_pushboolean(L,isnormal(A(1)));
+ return 1;
+}
+
+static int Llgamma(lua_State *L)		/** lgamma */
+{
+ lua_pushnumber(L,l_mathop(lgamma)(A(1)));
+ return 1;
+}
+
+static int Llog(lua_State *L)			/** log */
+{
+ int n=lua_gettop(L);
+ if (n==1)
+  lua_pushnumber(L,l_mathop(log)(A(1)));
+ else
+ {
+  lua_Number b=A(2);
+  if (b==10.0)
+   lua_pushnumber(L,l_mathop(log10)(A(1)));
+  else if (b==2.0)
+   lua_pushnumber(L,l_mathop(log2)(A(1)));
+  else
+   lua_pushnumber(L,l_mathop(log)(A(1))/l_mathop(log)(b));
+ }
+ return 1;
+}
+
+static int Llog10(lua_State *L)			/** log10 */
+{
+ lua_pushnumber(L,l_mathop(log10)(A(1)));
+ return 1;
+}
+
+static int Llog1p(lua_State *L)			/** log1p */
+{
+ lua_pushnumber(L,l_mathop(log1p)(A(1)));
+ return 1;
+}
+
+static int Llog2(lua_State *L)			/** log2 */
+{
+ lua_pushnumber(L,l_mathop(log2)(A(1)));
+ return 1;
+}
+
+static int Llogb(lua_State *L)			/** logb */
+{
+ lua_pushnumber(L,l_mathop(logb)(A(1)));
+ return 1;
+}
+
+static int Lnearbyint(lua_State *L)		/** nearbyint */
+{
+ lua_pushnumber(L,l_mathop(nearbyint)(A(1)));
+ return 1;
+}
+
+static int Lnextafter(lua_State *L)		/** nextafter */
+{
+ lua_pushnumber(L,l_mathop(nextafter)(A(1),A(2)));
+ return 1;
+}
+
+static int Lpow(lua_State *L)			/** pow */
+{
+ lua_pushnumber(L,l_mathop(pow)(A(1),A(2)));
+ return 1;
+}
+
+static int Lrad(lua_State *L)			/** rad */
+{
+ lua_pushnumber(L,rad(A(1)));
+ return 1;
+}
+
+static int Lremainder(lua_State *L)		/** remainder */
+{
+ lua_pushnumber(L,l_mathop(remainder)(A(1),A(2)));
+ return 1;
+}
+
+static int Lround(lua_State *L)			/** round */
+{
+ lua_pushnumber(L,l_mathop(round)(A(1)));
+ return 1;
+}
+
+static int Lscalbn(lua_State *L)		/** scalbn */
+{
+ lua_pushnumber(L,l_mathop(scalbn)(A(1),A(2)));
+ return 1;
+}
+
+static int Lsin(lua_State *L)			/** sin */
+{
+ lua_pushnumber(L,l_mathop(sin)(A(1)));
+ return 1;
+}
+
+static int Lsinh(lua_State *L)			/** sinh */
+{
+ lua_pushnumber(L,l_mathop(sinh)(A(1)));
+ return 1;
+}
+
+static int Lsqrt(lua_State *L)			/** sqrt */
+{
+ lua_pushnumber(L,l_mathop(sqrt)(A(1)));
+ return 1;
+}
+
+static int Ltan(lua_State *L)			/** tan */
+{
+ lua_pushnumber(L,l_mathop(tan)(A(1)));
+ return 1;
+}
+
+static int Ltanh(lua_State *L)			/** tanh */
+{
+ lua_pushnumber(L,l_mathop(tanh)(A(1)));
+ return 1;
+}
+
+static int Ltrunc(lua_State *L)			/** trunc */
+{
+ lua_pushnumber(L,l_mathop(trunc)(A(1)));
+ return 1;
+}
+
 static const luaL_Reg mathlib[] = {
   {"abs",   math_abs},
   {"acos",  math_acos},
@@ -741,8 +1097,226 @@ static const luaL_Reg mathlib[] = {
   {"huge", NULL},
   {"maxinteger", NULL},
   {"mininteger", NULL},
+  /* mathx */
+	{ "fabs",	Lfabs },
+	{ "acos",	Lacos },
+	{ "acosh",	Lacosh },
+	{ "asin",	Lasin },
+	{ "asinh",	Lasinh },
+	{ "atan",	Latan },
+	{ "atan2",	Latan2 },
+	{ "atanh",	Latanh },
+	{ "cbrt",	Lcbrt },
+	{ "ceil",	Lceil },
+	{ "copysign",	Lcopysign },
+	{ "cos",	Lcos },
+	{ "cosh",	Lcosh },
+	{ "deg",	Ldeg },
+	{ "erf",	Lerf },
+	{ "erfc",	Lerfc },
+	{ "exp",	Lexp },
+	{ "exp2",	Lexp2 },
+	{ "expm1",	Lexpm1 },
+	{ "fdim",	Lfdim },
+	{ "floor",	Lfloor },
+	{ "fma",	Lfma },
+	{ "fmax",	Lfmax },
+	{ "fmin",	Lfmin },
+	{ "fmod",	Lfmod },
+	{ "frexp",	Lfrexp },
+	{ "gamma",	Lgamma },
+	{ "hypot",	Lhypot },
+	{ "isfinite",	Lisfinite },
+	{ "isinf",	Lisinf },
+	{ "isnan",	Lisnan },
+	{ "isnormal",	Lisnormal },
+	{ "ldexp",	Lldexp },
+	{ "lgamma",	Llgamma },
+	{ "log",	Llog },
+	{ "log10",	Llog10 },
+	{ "log1p",	Llog1p },
+	{ "log2",	Llog2 },
+	{ "logb",	Llogb },
+	{ "modf",	Lmodf },
+	{ "nearbyint",	Lnearbyint },
+	{ "nextafter",	Lnextafter },
+	{ "pow",	Lpow },
+	{ "rad",	Lrad },
+	{ "remainder",	Lremainder },
+	{ "round",	Lround },
+	{ "scalbn",	Lscalbn },
+	{ "sin",	Lsin },
+	{ "sinh",	Lsinh },
+	{ "sqrt",	Lsqrt },
+	{ "tan",	Ltan },
+	{ "tanh",	Ltanh },
+	{ "trunc",	Ltrunc },
   {NULL, NULL}
 };
+
+/* RANDOM (lrandom) */
+#include "lua.h"
+#include "lauxlib.h"
+
+/* THIS SNIPPET IS FROM Mersenne Twister's random.c */
+#define N 624
+#define M 397
+#define MATRIX_A 0x9908b0dfUL   /* constant vector a */
+#define UPPER_MASK 0x80000000UL /* most significant w-r bits */
+#define LOWER_MASK 0x7fffffffUL /* least significant r bits */
+
+typedef struct {
+    unsigned long v[N]; /* the array for the state vector  */
+    int i;
+} MT;
+
+#define mt	(o->v)
+#define mti	(o->i)
+
+/* initializes mt[N] with a seed */
+static void init_genrand(MT *o, unsigned long s)
+{
+    mt[0]= s & 0xffffffffUL;
+    for (mti=1; mti<N; mti++) {
+        mt[mti] = 
+	    (1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti); 
+        /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+        /* In the previous versions, MSBs of the seed affect   */
+        /* only MSBs of the array mt[].                        */
+        /* 2002/01/09 modified by Makoto Matsumoto             */
+        mt[mti] &= 0xffffffffUL;
+        /* for >32 bit machines */
+    }
+}
+
+/* generates a random number on [0,0xffffffff]-interval */
+static unsigned long genrand_int32(MT *o)
+{
+    unsigned long y;
+    static unsigned long mag01[2]={0x0UL, MATRIX_A};
+    /* mag01[x] = x * MATRIX_A  for x=0,1 */
+
+    if (mti >= N) { /* generate N words at one time */
+        int kk;
+
+        if (mti == N+1)   /* if init_genrand() has not been called, */
+            init_genrand(o,5489UL); /* a default initial seed is used */
+
+        for (kk=0;kk<N-M;kk++) {
+            y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
+            mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1UL];
+        }
+        for (;kk<N-1;kk++) {
+            y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
+            mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
+        }
+        y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
+        mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1UL];
+
+        mti = 0;
+    }
+  
+    y = mt[mti++];
+
+    /* Tempering */
+    y ^= (y >> 11);
+    y ^= (y << 7) & 0x9d2c5680UL;
+    y ^= (y << 15) & 0xefc60000UL;
+    y ^= (y >> 18);
+
+    return y;
+}
+
+/* generates a random number on [0,1) with 53-bit resolution*/
+static double genrand(MT *o) 
+{ 
+    unsigned long a=genrand_int32(o)>>5, b=genrand_int32(o)>>6; 
+    return(a*67108864.0+b)*(1.0/9007199254740992.0); 
+} 
+
+#define AUTHOR "Mersenne Twister MT19937"
+/*************************************/
+#define RTYPE		"random"
+
+#define SEED		2018UL
+
+static MT *Pget(lua_State *L, int i)
+{
+ return luaL_checkudata(L,i,RTYPE);
+}
+
+static MT *Pnew(lua_State *L)
+{
+ MT *c=lua_newuserdata(L,sizeof(MT));
+ luaL_newmetatable(L,RTYPE);
+  lua_setmetatable(L,-2);
+
+ return c;
+}
+
+static int Lnew(lua_State *L)			/** new([seed]) */
+{
+ lua_Number seed=luaL_optnumber(L,1,SEED);
+ MT *c=Pnew(L);
+ init_genrand(c,seed);
+ return 1;
+}
+
+static int Lclone(lua_State *L)			/** clone(c) */
+{
+ MT *c=Pget(L,1);
+ MT *d=Pnew(L);
+ *d=*c;
+ return 1;
+}
+
+static int Lseed(lua_State *L)			/** seed(c,[seed]) */
+{
+ MT *c=Pget(L,1);
+ init_genrand(c,luaL_optnumber(L,2,SEED));
+ lua_settop(L,1);
+ return 1;
+}
+
+static int Lvalue(lua_State *L)			/** value(c,[a,b]) */
+{
+ MT *c=Pget(L,1);
+ double a,b,r=genrand(c);
+ switch (lua_gettop(L))
+ {
+  case 1:
+   lua_pushnumber(L,r);
+   return 1;
+  case 2:
+   a=1;
+   b=luaL_checknumber(L,2);
+   break;
+  default:
+   a=luaL_checknumber(L,2);
+   b=luaL_checknumber(L,3);
+   break;
+ }
+ if (a>b) { double t=a; a=b; b=t; }
+ a=ceil(a);
+ b=floor(b);
+ if (a>b) return 0;
+ r=a+floor(r*(b-a+1));
+ lua_pushnumber(L,r);
+ return 1;
+}
+
+#define MYTOSTRING
+
+static const luaL_Reg Random[] =
+{
+	MYTOSTRING
+	{ "clone",	Lclone		},
+	{ "new",	Lnew		},
+	{ "seed",	Lseed		},
+	{ "value",	Lvalue		},
+	{ NULL,		NULL		}
+};
+
 
 
 /*
@@ -759,6 +1333,11 @@ LUAMOD_API int luaopen_math (lua_State *L) {
   lua_pushinteger(L, LUA_MININTEGER);
   lua_setfield(L, -2, "mininteger");
   setrandfunc(L);
+  lua_pushnumber(L,INFINITY);	lua_setfield(L,-2,"inf");
+  lua_pushnumber(L,NAN);		lua_setfield(L,-2,"nan");
+  
+  luaL_newlib(L, Random);
+  lua_setfield(L, -2, "rand");
+
   return 1;
 }
-

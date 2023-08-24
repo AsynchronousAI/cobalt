9737e90825ae5729a4f591f34f72977b080ee132
AsynchronousAI <aarav.sethi@outlook.com>
2023-08-24 17:12:31 -0500

    Unix syslog

--- README.md
+++ README.md
@@ -20,3 +20,7 @@ Currently up to date with Lua `5.4.3`.
 - Enums
 - Records (like teal records)
 - Traceback mode for C by using: `#include <ltrace.c> && #include <ltrace.h>` over `#include <cobalt.h> && #include <cobalt.h>`
+
+## Todo
+- add credits for syslog
+- add system log to examples
--- cobalt23/src/lcinterface.c
+++ cobalt23/src/lcinterface.c
@@ -291,6 +291,150 @@ static const struct luaL_Reg lcinterface_memory_lib[] = {
   {"realloc", resize_memory},
   {NULL, NULL}
 };
+#if defined __unix__ || LUA_USE_POSIX || __APPLE__
+// Provides Unix specific functions
+
+// The following snippet is taken and modified from LuaSystemLog
+/*
+** LuaSystemLog
+** Copyright 1994-2021 Nicolas Casalini (DarkGod)
+**
+*/
+
+#include <syslog.h>
+
+static int lsyslog_open(lua_State *L)
+{
+	const char *ident = luaL_checkstring(L, 1);
+	int facility = luaL_checkinteger(L, 2);
+
+	lua_pushvalue(L,1);
+	lua_setfield(L, LUA_REGISTRYINDEX, "cobaltlog:ident");
+	openlog(ident, LOG_PID, facility);
+	return 0;
+}
+
+static int lsyslog_log(lua_State *L)
+{
+	int level = luaL_checkinteger(L, 1);
+	const char *line = luaL_checkstring(L, 2);
+
+	syslog(level, "%s", line);
+	return 0;
+}
+
+static int lsyslog_close(lua_State *L)
+{
+	closelog();
+	lua_pushnil(L);
+	lua_setfield(L, LUA_REGISTRYINDEX, "cobaltlog:ident");
+	return 0;
+}
+
+/*
+** Assumes the table is on top of the stack.
+*/
+static void set_info (lua_State *L)
+{
+  lua_pushliteral(L, "FACILITY_AUTH");
+	lua_pushnumber(L,  LOG_AUTH);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_AUTHPRIV");
+	lua_pushnumber(L,  LOG_AUTHPRIV);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_CRON");
+	lua_pushnumber(L,  LOG_CRON);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_DAEMON");
+	lua_pushnumber(L,  LOG_DAEMON);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_FTP");
+	lua_pushnumber(L,  LOG_FTP);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_KERN");
+	lua_pushnumber(L,  LOG_KERN);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_LPR");
+	lua_pushnumber(L,  LOG_LPR);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_MAIL");
+	lua_pushnumber(L,  LOG_MAIL);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_NEWS");
+	lua_pushnumber(L,  LOG_NEWS);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_SYSLOG");
+	lua_pushnumber(L,  LOG_SYSLOG);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_USER");
+	lua_pushnumber(L,  LOG_USER);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_UUCP");
+	lua_pushnumber(L,  LOG_UUCP);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_LOCAL0");
+	lua_pushnumber(L,  LOG_LOCAL0);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_LOCAL1");
+	lua_pushnumber(L,  LOG_LOCAL1);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_LOCAL2");
+	lua_pushnumber(L,  LOG_LOCAL2);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_LOCAL3");
+	lua_pushnumber(L,  LOG_LOCAL3);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_LOCAL4");
+	lua_pushnumber(L,  LOG_LOCAL4);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_LOCAL5");
+	lua_pushnumber(L,  LOG_LOCAL5);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_LOCAL6");
+	lua_pushnumber(L,  LOG_LOCAL6);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "FACILITY_LOCAL7");
+	lua_pushnumber(L,  LOG_LOCAL7);
+	lua_settable(L, -3);
+
+	lua_pushliteral(L, "LOG_EMERG");
+	lua_pushnumber(L,  LOG_EMERG);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "LOG_ALERT");
+	lua_pushnumber(L,  LOG_ALERT);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "LOG_CRIT");
+	lua_pushnumber(L,  LOG_CRIT);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "LOG_ERR");
+	lua_pushnumber(L,  LOG_ERR);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "LOG_WARNING");
+	lua_pushnumber(L,  LOG_WARNING);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "LOG_NOTICE");
+	lua_pushnumber(L,  LOG_NOTICE);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "LOG_INFO");
+	lua_pushnumber(L,  LOG_INFO);
+	lua_settable(L, -3);
+	lua_pushliteral(L, "LOG_DEBUG");
+	lua_pushnumber(L,  LOG_DEBUG);
+	lua_settable(L, -3);
+}
+
+static const struct luaL_Reg lcinterface_unix_lib[] = {
+  {"open", lsyslog_open},
+	{"close", lsyslog_close},
+	{"log", lsyslog_log},
+	{NULL, NULL},
+};
+#elif defined _WIN32 || LUA_USE_WINDOWS || LUA_USE_MINGW
+// Provides Windows specific functions
+static const struct luaL_Reg lcinterface_win_lib[] = {
+  {NULL, NULL}
+};
+#endif
 
 LUALIB_API int luaopen_lcinterface(lua_State* L) {
   luaL_newlib(L, lcinterface_lib);
@@ -298,6 +442,13 @@ LUALIB_API int luaopen_lcinterface(lua_State* L) {
   lua_setfield(L, -2, "memory");
   luaL_newlib(L, lcinterface_error_lib);
   lua_setfield(L, -2, "error");
+  #if defined __unix__ || LUA_USE_POSIX || __APPLE__
+  luaL_newlib(L, lcinterface_unix_lib);
+  lua_setfield(L, -2, "unix");
+  #elif defined _WIN32 || LUA_USE_WINDOWS || LUA_USE_MINGW
+  luaL_newlib(L, lcinterface_win_lib);
+  lua_setfield(L, -2, "win");
+  #endif
 
   return 1;
 }

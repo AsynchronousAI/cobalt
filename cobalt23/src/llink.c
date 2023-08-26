c1a9ff7d9f181a0a97a02e219369a38b08ab6077
AsynchronousAI <aarav.sethi@outlook.com>
2023-08-26 09:26:06 -0500

    link

--- cobalt23/src/llink.c
+++ cobalt23/src/llink.c
@@ -7,3 +7,9 @@
 #include "cobalt.h"
 #include "lauxlib.h"
 #include "lualib.h"
+
+#define link(file, name) \
+static int luaopen_##name(lua_State* L) { \
+    luaL_dofile(L, file); \
+    return 1; \
+}
